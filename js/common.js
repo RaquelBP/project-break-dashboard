0f5cc4f2f0ba18acbd69b88f05f5008f86a468d4
Raquel Benito <raquelbenitop@gmail.com>
2024-04-09 00:05:53 +0200

    fix: problem with background images popping during transitions

--- js/common.js
+++ js/common.js
@@ -1,7 +1,12 @@
 function backgroundImg(){
     const randomNum= Math.floor(Math.random()*10)
     const imageUrl = `/img/background_0${randomNum}.jpg`;
-    document.body.style.backgroundImage = `url("${imageUrl}")`;
+    //Carga la imagen para evitar problemas con la transición entre imagenes
+    const img = new Image()  //Crea un objeto imagen que hace que se cargue la imagen en el caché del navegador
+    img.src = imageUrl   //Setea el src del objeto imagen que acabamos de crear con el url creado antes
+    img.onload = () => {   //onload es un evento que hace que se ejecute el código de dentro una vez la imagen se ha cargado por completo 
+        document.body.style.backgroundImage = `url("${imageUrl}")`
+    }
 }
 
 function pageEventListeners(){
