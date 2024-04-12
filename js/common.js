4aa493c787285c0e2204a26b038d16985cabcfa1
Raquel Benito <raquelbenitop@gmail.com>
2024-04-12 17:10:52 +0200

    fix: paths

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 function backgroundImg(){
     const randomNum= Math.floor(Math.random()*10)
-    const imageUrl = `/img/background_0${randomNum}.jpg`;
+    const imageUrl = `./img/background_0${randomNum}.jpg`;
     //Carga la imagen para evitar problemas con la transición entre imagenes
     const img = new Image()  //Crea un objeto imagen que hace que se cargue la imagen en el caché del navegador
     img.src = imageUrl   //Setea el src del objeto imagen que acabamos de crear con el url creado antes
@@ -12,20 +12,20 @@ function backgroundImg(){
 function pageEventListeners(){
     if(document.getElementById("home")){
         document.getElementById("home").addEventListener("click", ()=>{
-            window.open("/index.html", "_self")
+            window.open("./index.html", "_self")
         })
     }
     document.getElementById("digital-clock-page").addEventListener("click", ()=>{
-        window.open("/digital-clock.html", "_self")
+        window.open("./digital-clock.html", "_self")
     })
     document.getElementById("key-generator-page").addEventListener("click", ()=>{
-        window.open("/key-generator.html", "_self")
+        window.open("./key-generator.html", "_self")
     })
     document.getElementById("list-page").addEventListener("click", ()=>{
-        window.open("/list.html", "_self")
+        window.open("./list.html", "_self")
     })
     document.getElementById("weather-page").addEventListener("click", ()=>{
-        window.open("/weather.html", "_self")
+        window.open("./weather.html", "_self")
     })
 }
 
